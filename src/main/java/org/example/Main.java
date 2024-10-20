30eec35ccfdc0bcf55bc358e10ad622463657cb8
nataliieb <164919545+nataliieb@users.noreply.github.com>
2024-10-20 19:17:18 +0200

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {System.out.println("Push test");
-        System.out.println("Pull test2");
+        System.out.println("Pull test2  ");
     }
 }
